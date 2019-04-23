6147be0baf5659273a9454f1f8941d41adf1738f
Guest User <Guest@epicodus-94.local>
2019-04-23 09:53:04 -0700

    homework finished

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -4,7 +4,20 @@ import PropTypes from "prop-types";
 function Ticket(props){
   return (
     <div>
-      <h3>{props.location} - {props.names}</h3>
+
+      <style jsx>{`
+        div {
+          background-color: #9fbff2;
+        }
+        div:hover {
+          background-color: #85aff2;
+        }
+        .example{
+          color: #6d5cf2;
+        }
+      `}</style>
+
+    <h3 className="example">{props.location} - {props.names}</h3>
       <p><em>{props.issue}</em></p>
       <hr/>
     </div>
